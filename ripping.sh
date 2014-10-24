757b1aae95d7dcc6f7179610a485b31a47abe696
Roberto 'Rex' Sigalotti <rexrock@gmail.com>
2014-10-24 18:28:29 +0200

    add support for custom subtitles

--- ripping.sh
+++ ripping.sh
@@ -3,13 +3,18 @@
 base_dir = ARGV[0]
 preset = ARGV[1]
 ext = ARGV[2]
-command  = "HandBrakeCLI -i '{{input_file}}' -o '{{output_file}}.mp4' --preset='#{preset}'"
+audio = ARGV[3]
+command  = "HandBrakeCLI -i '{{input_file}}' -o '{{output_file}}.mp4' --preset='#{preset}' -a #{audio} --srt-file {{srt_string}} "
 
 Dir[ "#{base_dir}/*.#{ext}" ].each do |video|
   file_name = File.basename(video)
+  only_file_name = File.basename(video, '.*').gsub("ITA.ENG", "ITA_ENG")
   simple_name = File.basename( video, File.extname(video) )
 
-  str_cmd = command.gsub("{{input_file}}", "#{base_dir}/#{file_name}").gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}")
+
+  str_cmd = command.gsub("{{input_file}}", "#{base_dir}/#{file_name}")
+  						.gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}")
+  						.gsub("{{srt_string}}", "#{base_dir}/#{only_file_name}.it.srt,#{base_dir}/#{only_file_name}.en.srt")
   puts str_cmd
   if !Dir.exists?( "#{base_dir}/ripped" )
     Dir.mkdir( "#{base_dir}/ripped" )
