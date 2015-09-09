670aaeb627ef271c3bdd8364edb24e00352ab1ce
AUTHORNAME <AUTHOREMAIL>
2015-09-09 16:56:49 +0200

    fix for no subtitle

--- ripping.sh
+++ ripping.sh
@@ -8,28 +8,23 @@ preset = ARGV[1]
 ext = ARGV[2]
 audio = ARGV[3]
 
-command  = "HandBrakeCLI -i '{{input_file}}' -o '{{output_file}}.mp4' --preset='#{preset}' --srt-file {{srt_string}} -a #{audio}"
-
-puts base_dir
+cmd  = "HandBrakeCLI -i '{{input_file}}' -o '{{output_file}}.mp4' --preset='#{preset}' --srt-file {{srt_string}} -a #{audio}"
+cmd_no_subtitle = "HandBrakeCLI -i '{{input_file}}' -o '{{output_file}}.mp4' --preset='#{preset}' -a #{audio}"
 
 Dir[ "#{base_dir}/*.#{ext}" ].each do |video|
-file_name = File.basename(video)
-only_file_name = File.basename(video, '.*')
-simple_name = File.basename( video, File.extname(video) )
+  file_name = File.basename(video)
+  only_file_name = File.basename(video, '.*')
+  simple_name = File.basename( video, File.extname(video) )
 
 
-if subtitle == "none"
-  str_cmd = command.gsub("{{input_file}}", "#{base_dir}/#{file_name}").gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}")
-else
-  str_cmd = command.gsub("{{input_file}}", "#{base_dir}/#{file_name}").gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}").gsub("{{srt_string}}", "#{base_dir}/#{only_file_name}.it.srt,#{basE_dir}/#{only_file_name}.en.srt")
+  if subtitle == "none"
+    str_cmd = cmd_no_subtitle.gsub("{{input_file}}", "#{base_dir}/#{file_name}").gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}")
+  else
+    str_cmd = cmd.gsub("{{input_file}}", "#{base_dir}/#{file_name}").gsub("{{output_file}}", "#{base_dir}/ripped/#{simple_name}").gsub("{{srt_string}}", "#{base_dir}/#{only_file_name}.it.srt,#{basE_dir}/#{only_file_name}.en.srt")
   end
-
-
-  puts str_cmd
 
   if !Dir.exists?( "#{base_dir}/ripped" )
     Dir.mkdir( "#{base_dir}/ripped" )
-    end
-    system "#{str_cmd}"
-    end
-
+  end
+  system "#{str_cmd}"
+end
